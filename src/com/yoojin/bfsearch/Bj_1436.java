f16553ae739289b5cbcb39d57782f8b6394037e1
yoojin Jang <yoojinjangjang@gmail.com>
2023-01-30 07:32:53 +0900

    search/bj_1436.java

--- src/com/yoojin/bfsearch/Bj_1436.java
+++ src/com/yoojin/bfsearch/Bj_1436.java
@@ -1,7 +1,18 @@
 package com.yoojin.bfsearch;
 
+import java.util.Scanner;
+
 public class Bj_1436 {
 	public static void main(String[] args) {
-		
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int num = 0;
+		while(N > 0) {
+			num += 1;
+			if (String.valueOf(num).contains("666")) {
+				N -= 1;
+			}
+		}
+		System.out.println(num);
 	}
 }
